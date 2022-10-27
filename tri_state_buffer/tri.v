13859d42261b0f880ee872d1c5b2199e4c2e0849
Rupak Saha <rupak10987@gmail.com>
2022-10-27 22:04:59 +0600

    n

--- tri_state_buffer/tri.v
+++ tri_state_buffer/tri.v
@@ -1,9 +1,10 @@
-module tri (
+module tris 
+(
     input wire oe,
     input wire [3:0] s_in,
-    input wire [3:0] b,
+    inout wire [3:0] b,
     output wire [3:0] s_out
 );
-b=(oe==1'b1)?s_in:4'bzzzz;
+assign b=(oe==1'b1)?s_in:4'bzzzz; 
 assign s_out=b;    
 endmodule
--- tri_state_buffer/tri_tb.v
+++ tri_state_buffer/tri_tb.v
@@ -1,10 +1,10 @@
 `timescale 1ns/1ps
 module tri_tb;
-wire[3:0]b;
+reg[3:0]b;
 reg[3:0]s_in;
 reg oe;
 wire[3:0]s_out;
-tri uut(.s_in(s_in), .s_out(s_out), .oe(oe), .b(b));
+tris uut(.s_in(s_in), .s_out(s_out), .oe(oe), .b(b));
 initial begin
     $dumpfile("test.vcd");
     $dumpvars(0,tri_tb);
